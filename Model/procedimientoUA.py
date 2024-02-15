eac9b7a61419f39c3d6cce4310a88af5e594a0b0
SrThiagoXll <xephos239xll@hotmail.com>
2024-02-14 19:19:00 -0500

    Quitando Clase sin usar

--- Model/procedimientoUA.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from Model.Actividad import ActividadBase
-from Model.Usuario import UsuarioBase
-
-
-class UsuarioActividad():
-    
-    actividad:ActividadBase
-    usuario:UsuarioBase
