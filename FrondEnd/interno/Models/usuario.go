97c7e9e726fe85c68258e56f8da325c6b237e11a
SrThiagoXll <146996846+SrThiagoXll@users.noreply.github.com>
2026-01-29 00:26:40 -0500

    Delete FrondEnd directory

--- FrondEnd/interno/Models/usuario.go
+++
@@ -1,9 +0,0 @@
-package Models
-
-type Usuario struct {
-	Nombre_Usuario  string `json:"Nombre_Usuario"`
-	Nombre_Completo string `json:"Nombre_Completo"`
-	Correo          string `json:"Correo"`
-	Contraseña      string `json:"Contraseña"`
-	Rol             string `json:"Rol"`
-}
